ab434bec938f1de450c3a1346a072b3d46d59f15
Carlos López Solano <carloslopezels@gmail.com>
2021-03-08 21:01:21 -0600

    progress

--- stringcalculator.py
+++ stringcalculator.py
@@ -2,15 +2,23 @@ from re import split
 
 
 def add(raw_content):
+
   # Case 1: empty string
   if len(raw_content) == 0: return 0
 
+  current_delimiter = ',|\n'
+  content = raw_content
+
+  # Delimeter detection
+  if len(raw_content) >= 3 and raw_content[1] == '\n':
+    current_delimiter = raw_content[0]
+    content = raw_content[2:]
+
   # Case 2: one or more arguments
-  default_delimiter = ',|\n'
-  contents = split(default_delimiter, raw_content)
-  for element in contents:
+  split_content = split(current_delimiter, content)
+  for element in split_content:
     if not element.isnumeric():
       raise ValueError("only integers allowed - {}".format(element))
 
-  numeric_contents = [int(number) for number in contents]
+  numeric_contents = [int(number) for number in split_content]
   return sum(numeric_contents)
