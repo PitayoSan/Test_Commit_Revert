d56fa686d4da0399e4196a5941aca0409194445b
Carlos López Solano <carloslopezels@gmail.com>
2021-03-08 20:02:37 -0600

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -8,5 +8,17 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(stringcalculator.add(""), 0)
 
 
+    def test_one_arg(self):
+        self.assertEqual(stringCalculator.add("1"), 1)
+
+
+    def test_two_args(self):
+        self.assertEqual(stringCalculator.add("1,2"), 3)
+
+
+    def test_non_numeric_value(self):
+        self.assertRaises(ValueError, stringCalculator.add("1,a"))
+
+
 if __name__ == '__main__':
     unittest.main()
