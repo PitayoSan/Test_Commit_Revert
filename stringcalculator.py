6cf9a0cba9e87dd420de075bc9436d837b134706
Carlos López Solano <carloslopezels@gmail.com>
2021-03-08 20:18:02 -0600

    progress

--- stringcalculator.py
+++ stringcalculator.py
@@ -5,3 +5,8 @@ def add(raw_content):
   # Case 1: empty string
   if len(raw_content) == 0: return 0
 
+  # Case 2: one or more arguments
+  contents = raw_content.split(',')
+  for element in contents:
+    if not element.isnumeric():
+      raise ValueError("only integers allowed - {}".format(element))
